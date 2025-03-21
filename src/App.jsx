432ead285afab88d2550c125311073f33fc63791
Ivan <tushchukivan@gmail.com>
2025-03-21 19:33:31 +0200

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -15,94 +15,90 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-
-
 export const App = () => {
   const [good, setGoods] = useState(goodsFromServer);
   const [isReversed, setIsReversed] = useState(false);
   const [sortType, setSortType] = useState('none');
 
   const sortAlphabet = () => {
-    if (setSortType !== 'alphabet') {
-      const sorting = [...good].sort()
-      setGoods(sorting)
-      setSortType('alphabet')
-    } else{
-      setGoods(goodsFromServer);
-      setSortType('none')
-    }
-  }
-
-  const sortByLength = () => {
-    if(setSortType !== 'length'){
-      const byLength = [...good].sort((a,b) => a.length - b.length)
-      setGoods(byLength)
-      setSortType('length')
+    if (sortType !== 'alphabet') {
+      const sorting = [...good].sort();
+      setGoods(sorting);
+      setSortType('alphabet');
     } else {
       setGoods(goodsFromServer);
-      setSortType('none')
+      setSortType('none');
     }
-  }
+  };
+
+  const sortByLength = () => {
+    if (sortType !== 'length') {
+      const byLength = [...good].sort((a, b) => a.length - b.length);
+      setGoods(byLength);
+      setSortType('length');
+    } else {
+      setGoods(goodsFromServer);
+      setSortType('none');
+    }
+  };
 
   const setReverse = () => {
-    const reversing = [...good].reverse()
+    const reversing = [...good].reverse();
     setGoods(reversing);
-    setIsReversed(!isReversed)
-  }
+    setIsReversed(!isReversed);
+  };
 
   const setReset = () => {
     setGoods(goodsFromServer);
-    setSortType('none')
-    setIsReversed(false)
-  }
+    setSortType('none');
+    setIsReversed(false);
+  };
 
   const goodList = good.map((good, index) => {
-    return (<li
-      key={index}
-      data-cy="Good">{good}</li>)
-  })
+    return (
+      <li key={index} data-cy="Good">
+        {good}
+      </li>
+    );
+  });
 
   return (
     <div className="section content">
-    <div className="buttons">
-      <button
-      type="button"
-      className="button is-info is-light"
-      onClick = {sortAlphabet}
-      >
-        Sort alphabetically
-      </button>
+      <div className="buttons">
+        <button
+          type="button"
+          className={`button is-info ${sortType === 'alphabet' ? '' : 'is-light'}`}
+          onClick={sortAlphabet}
+        >
+          Sort alphabetically
+        </button>
 
-      <button
-      type="button"
-      className="button is-success is-light"
-      onClick={sortByLength}
-      >
-        Sort by length
-      </button>
+        <button
+          type="button"
+          className={`button is-success ${sortType === 'length' ? '' : 'is-light'}`}
+          onClick={sortByLength}
+        >
+          Sort by length
+        </button>
 
-      <button
-      type="button"
-      className="button is-warning is-light"
-      onClick={setReverse}
-      >
-        Reverse
-      </button>
+        <button
+          type="button"
+          className={`button is-warning ${isReversed ? '' : 'is-light'}`}
+          onClick={setReverse}
+        >
+          Reverse
+        </button>
 
-      <button
-      type="button"
-      className="button is-danger is-light"
-      onClick={setReset}
-      >
-        Reset
-      </button>
+        <button
+          type="button"
+          className={`button is-danger ${sortType === 'none' && !isReversed ? '' : 'is-light'}`}
+          onClick={setReset}
+        >
+          Reset
+        </button>
+      </div>
+
+      <ul>{goodList}</ul>
     </div>
-
-
-
-    <ul>
-      {goodList}
-    </ul>
-  </div>
-  )
+  );
 };
